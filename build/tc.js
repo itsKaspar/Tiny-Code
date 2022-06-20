bb0807a849396388bfd03a7ea67cf9e4c8084f1f
itsKaspar <kaspar.ravel@gmail.com>
2022-06-20 12:26:58 +0200

    fixing css style gen

--- build/tc.js
+++ build/tc.js
@@ -30165,10 +30165,7 @@
   });
 
   class TinyCode{
-    constructor(code, layout = "side"){
-      this.layout = this.hasLayout() || layout;
-      this.setLayout(); // change/add the css of <style id="pageStyle"></style>
-      
+    constructor(code){
       this.code = this.hasHashCode() || code;
       let context = this;
       this.editor = new EditorView({
@@ -30205,43 +30202,6 @@
         return false;
       }
     }
-
-    hasLayout(){
-      const queryString = window.location.search; // get url
-      const urlParams = new URLSearchParams(queryString); // get url parameters
-      return urlParams.get('layout');
-    }
-
-      setLayout(){
-        const pageStyle = document.getElementsByTagName("style")[0];
-
-        // # Define different style
-        let cssTop = `#editor{ width:100%;height:50%; }         #p5{ top:50%;border:0;width:100%;height:50%; }`;
-        let cssBot = `#editor{ width:100%;height:50%;top:50%; } #p5{ top:0%;border:0;width:100%;height:50%; } `;
-        let cssLeft = `#editor{ height:100%;width:50%;left:0%;top:0%;} #p5{ left:50%;top:0;height:100%;width:50%; }`;
-        let cssRight = `#editor{ height:100%;width:50%;left:50%;top:0%;} #p5{ left:0%;top:0;height:100%;width:50%; }`;
-        let cssCode = `#editor{width:100%;height:100%;}#p5 {display:none;}`;
-        let cssVisual = `#editor {display:none;}#p5{ left:0%;top:0;height:100%;width:100%; }`;
-        let cssOverlay = `#editor {width:100%;height:100%;top0;left:0} #p5 {width:100%;height:100%;top0;left:0}`;
-
-
-        switch(this.layout){
-          case "top":    pageStyle.innerHTML += cssTop; break;
-          case "bot":    pageStyle.innerHTML += cssBot; break;
-          case "left":   pageStyle.innerHTML += cssLeft; break;
-          case "right":  pageStyle.innerHTML += cssRight; break;
-          case "code":   pageStyle.innerHTML += cssCode; break;
-          case "visual": pageStyle.innerHTML += cssVisual; break;
-          case "overlay":pageStyle.innerHTML += cssOverlay; break;
-          default :      pageStyle.innerHTML += cssLeft; break;
-        }
-
-        let css2 = `.cm-editor{
-        background-color:red;
-      }`;
-        pageStyle.innerHTML += css2;
-    }
-
 
     createIframe(code){
           let iframe = document.createElement('iframe');
