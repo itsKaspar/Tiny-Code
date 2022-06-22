61d865997c19a92c6fb1ba5b33fcc5b7b735b2b7
itsKaspar <kaspar.ravel@gmail.com>
2022-06-22 14:25:44 +0200

    updated thibngs

--- editor.js
+++ editor.js
@@ -24,7 +24,7 @@ function draw(){
 
 class TinyCode{
   constructor(parent, code=DEFAULT_CODE, layout = "side"){
-    console.log("constructing text editor")
+    // console.log("constructing text editor")
     // set layout
     this.layout = this.hasLayout() || layout;
     this.setLayout(); // change/add the css of <style id="pageStyle"></style>
--- tinycode.js
+++
@@ -1,1 +0,0 @@
-// const pako = require('pako');
